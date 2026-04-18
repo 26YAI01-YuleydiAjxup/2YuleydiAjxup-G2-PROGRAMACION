7d4782b7696b436c1b39962ac406efb2ab4eb7e4
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-18 17:03:05 -0600

    Add files via upload

---
+++ Ejercicio5enClase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_en_clase
+{
+    internal class Ejercicio5enClases
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meses del año");
+            Console.Write("Ingrese numero de mes: ");
+            int mes = Convert.ToInt32(Console.ReadLine());
+
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("Enero - 31 dias");
+                    break;
+                case 2:
+                    Console.WriteLine("Febrero - 28 dias");
+                    break;
+                case 3:
+                    Console.WriteLine("Marzo - 31 dias");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril - 30 dias");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayo - 31 dias");
+                    break;
+                case 6:
+                    Console.WriteLine("Junio - 30 dias");
+                    break;
+                case 7:
+                    Console.WriteLine("Julio - 31 dias");
+                    break;
+                case 8:
+                    Console.WriteLine("Agosto - 31 dias");
+                    break;
+                case 9:
+                    Console.WriteLine("Septiembre - 30 dias");
+                    break;
+                case 10:
+                    Console.WriteLine("Octubre - 31 dias");
+                    break;
+                case 11:
+                    Console.WriteLine("Noviembre - 30 dias");
+                    break;
+                case 12:
+                    Console.WriteLine("Diciembre - 31 dias");
+                    break;
+                default:
+                    Console.WriteLine("Escribir un número de mes valido");
+                    break;
+            }
+        }
+    }
+}
