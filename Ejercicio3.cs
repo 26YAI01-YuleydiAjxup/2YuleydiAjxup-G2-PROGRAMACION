cc1cedf4536f2ebcdc5144c3c6e100996aab5a4b
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-18 12:48:22 -0600

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejecicio3_de_Clase
+{
+    internal class Ejercicio3 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clasificacion de letras");
+            Console.Write("Dame una letra: ");
+            char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (letra)
+            {
+                case 'A':
+                    Console.WriteLine("Excelente");
+                    break;
+                case 'B':
+                    Console.WriteLine("Bueno");
+                    break;
+                case 'C':
+                    Console.WriteLine("Regular");
+                    break;
+                case 'D':
+                    Console.WriteLine("Deficiente");
+                    break;
+                case 'F':
+                    Console.WriteLine("Reprobado");
+                    break;
+                default:
+                    Console.WriteLine("Digite una letra entre A,B,C,D,F");
+                    break;
+            }
+        }
+    }
+}
