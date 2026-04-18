61b6f32bf934593d9b2f507202589ded78bf58ae
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-18 12:29:31 -0600

    Add files via upload

---
+++ DiadelaSemana.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_de_la_semana
+{
+    internal class DiadelaSemana
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine("DIAS DE LA SEMANA");
+            Console.WriteLine("*********************");
+
+            int dia;
+            Console.Write("Digite unvalor entero del 1 al 7: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            switch (dia)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miercoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sabado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+                default:
+                    Console.WriteLine("Digite un numero entre 1 y 7");
+                    break;
+            }
+    }
+}
+}
