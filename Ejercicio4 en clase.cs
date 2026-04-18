6a1f24a6cd067d5154e6df501be1f1683f95b35a
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-18 15:50:03 -0600

    Add files via upload

---
+++ Ejercicio4 en clase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4_en_Clase
+{
+    internal class Ejercicio4enClase
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menú: ");
+            Console.WriteLine("1. Hamburguesa");
+            Console.WriteLine("2. Pizza");
+            Console.WriteLine("3. Pollo frito");
+            Console.WriteLine("4. Ensalada");
+
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Usted pidio Hamburguesa");
+                    break;
+                case 2:
+                    Console.WriteLine("Usted pidio Pizza");
+                    break;
+                case 3:
+                    Console.WriteLine("Usted pidio Pollo frito");
+                    break;
+                case 4:
+                    Console.WriteLine("Usted pidio Ensalada");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción del menú");
+                    break;
+            }
+        }
+    }
+}
